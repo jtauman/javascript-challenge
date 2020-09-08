dfc1c9b619d21edd34a3fc65ef497ed8ff600691
James Auman <jtauman@nc.rr.com>
2020-09-07 20:10:19 -0400

    appending ufo data to html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//Reference table body
+var tbody = d3.select("tbody");
+
+//loop through the data and get each ufoData object
+data.forEach((ufoData) => {
+    //append a row to tbody for each ufoData object
+    var row = tbody.append("tr");
+    //for each ufoData object get the key value pair
+    Object.entries(ufoData).forEach(([key, value]) => {
+        //append data cells for each row as table data
+        var cell = row.append("td");
+        //put in the ufoData object values into cells
+        cell.text(value);
+    });
+
+});
