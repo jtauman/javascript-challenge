5a7ac5b48aaa06b815b16f9dbab1d3fe61553f6d
James Auman <jtauman@nc.rr.com>
2020-09-08 14:19:54 -0400

    completed date filter in js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,3 +1,4 @@
+//appending data table to webpage
 // from data.js
 var tableData = data;
 
@@ -15,5 +16,57 @@ data.forEach((ufoData) => {
         //put in the ufoData object values into cells
         cell.text(value);
     });
-
 });
+
+
+//Search and filtering table on date/time
+
+//select button
+var button = d3.select("#filter-btn");
+
+//select the form
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Use the form input to filter the data by datetime
+    var filteredInput = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredInput);
+
+    
+    d3.select("tbody").selectAll("tr").remove();
+    //return the filtered values in the table
+    filteredInput.forEach((ufoData) => {
+        //append a row to tbody for each ufoData object
+        var row = tbody.append("tr");
+        //for each ufoData object get the key value pair
+        Object.entries(ufoData).forEach(([key, value]) => {
+            //append data cells for each row as table data
+            var cell = row.append("td");
+            //put in the ufoData object values into cells
+            cell.text(value);
+        });
+    });
+
+
+
+};
+
