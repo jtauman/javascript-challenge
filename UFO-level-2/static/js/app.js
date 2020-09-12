dc67d53cb784ba3c839a64492b778674f324884f
James Auman <jtauman@nc.rr.com>
2020-09-11 21:13:24 -0400

    added sorting based on date, city, state and shape

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -43,31 +43,48 @@ function runEnter() {
     d3.event.preventDefault();
 
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-
+    var inputElement1 = d3.select("#datetime.table-sort");
+    var inputElement2 = d3.select("#city.table-sort");
+    var inputElement3 = d3.select("#state.table-sort");
+    var inputElement4 = d3.select("#shape.table-sort");
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    var inputValue1 = inputElement1.property("value");
+    var inputValue2 = inputElement2.property("value");
+    var inputValue3 = inputElement3.property("value");
+    var inputValue4 = inputElement4.property("value");
 
-    console.log(inputValue);
+    console.log(inputValue1);
+    console.log(inputValue2);
+    console.log(inputValue3);
+    console.log(inputValue4);
     console.log(tableData);
 
     // Use the form input to filter the data by datetime
-    var filteredInput = tableData.filter(data => data.datetime === inputValue);
+    var filteredInput1 = tableData.filter(data => data.datetime === inputValue1);
+    var filteredInput2 = tableData.filter(data => data.city === inputValue2);
+    var filteredInput3 = tableData.filter(data => data.state === inputValue3);
+    var filteredInput4 = tableData.filter(data => data.shape === inputValue4);
 
-    console.log(filteredInput);
+    console.log(filteredInput1);
+    console.log(filteredInput2);
+    console.log(filteredInput3);
+    console.log(filteredInput4);
 
     //clear the table
     d3.select("tbody").selectAll("tr").remove();
 
 
-    if (Object.keys(filteredInput).length === 0) {
-        var text = d3.select("h3").text("Unfortunately your search did not return any data. Please select a date from 1/1/2010 - 1/13/2010.")
-    }
-    else { 
+    // if (Object.keys(filteredInput1).length === 0) {
+    //     var text = d3.select("h3").text("Unfortunately your search did not return any data. Please select a date from 1/1/2010 - 1/13/2010.")
+    // }
+    // else { 
     //run filteredInput through the getData function
-    getData(filteredInput);
+    getData(filteredInput1);
     d3.select("h3").text("");
-    }   
+    getData(filteredInput2);
+    getData(filteredInput3);
+    getData(filteredInput4);
+     
 };
 
 
